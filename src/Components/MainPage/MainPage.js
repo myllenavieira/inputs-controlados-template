2cd7e4493fa4e6d211611f6c56d36e266aaa806b
myllenavieira <vieirasilvamyllena@gmail.com>
2022-10-26 21:05:04 -0300

    Exercícios

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,24 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+  const [nome, setNome] = useState("")
+  const [idade, setIdade] = useState("")
+  const [email, setEmail] = useState("")
+
+  const onChangeNome = (event) => {
+    setNome(event.target.value);
+  }
+
+  const onChangeIdade = (event) => {
+    setIdade(event.target.value);
+  }
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const dadosSalvos = () => {
+    let dados = {
+      nome: nome,
+      idade: idade,
+      email: email
+    }
+    console.log(dados);
+    setNome("");
+    setIdade("");
+    setEmail("");
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input value={nome} onChange={onChangeNome}/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input value={idade} onChange={onChangeIdade}/>
         </label>
         <label>
           E-mail:
-          <Input />
+          <Input value={email} onChange={onChangeEmail}/>
         </label>
-      <button>Enviar dados</button>
+      <button onClick={dadosSalvos}>Enviar dados</button>
       </Form>
     </MainContainer>
   )
